0ab5823fd38d868fefc92d5c224b53c2dd2dca01
my file <vagifrg@code.edu.az>
2022-08-03 18:10:41 +0400

    new file

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name =  "salam usaqlar necesiz" ;
+            int count = 0;
+            for(int i = 0; i < name.Length; i++)
+            {
+                if (name[i] =='a')
+                {
+                    count++;
+                }
+            }
+            if (count >= 1)
+            {
+                Console.WriteLine("a herfi var");
+
+            }
+            else
+            {
+                Console.WriteLine("a herfi yoxdur");
+            }
+        }
+    }
+}
